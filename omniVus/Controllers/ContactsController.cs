0f2e13bd6f1293fe671042dcce03a9041c3e12cf
jessica <jessicamolin@hotmail.com>
2022-03-27 21:15:14 +0200

    Add project files.

---
+++ omniVus/Controllers/ContactsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace omniVus.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult BookMeeting()
+        {
+            return View();
+        }
+    }
+}
---
+++ omniVus/Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace omniVus.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
